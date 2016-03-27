43edae9e09f7b782a11dd79c16ae67c1b2aa43a7
iamforeverme <iamforeverme@163.com>
2016-03-27 16:46:28 +0800

    Version 1.0 with full function
    UI need to be further updated, but functions have been verified

--- src/correctWordUI.py
+++ src/correctWordUI.py
@@ -2,12 +2,19 @@ from Tkinter import *
 from WordRestore import SprintReview
 from OnlineDict import OnlineDict
 from json import JSONDecoder,JSONEncoder
+from os import linesep
+
 class Application(Frame):
-    def say_hi(self):
-        print "hi there, everyone!"
+    def overrideWordList(self):
+        data = self.__addContent.get("1.0",END)
+        self.__restorer.produceJSON(data,isAdd=False)
 
     def updateWords(self):
-        self.__dic.pronounce('bread')
+        # add / overwrite current dic
+        #self.__startReview.config(state="normal")
+        data = self.__addContent.get("1.0",END)
+        self.__restorer.produceJSON(data,isAdd=True)
+
 
     def startReview(self):
         def isRightWord(rightCount,reviewCount):
@@ -22,43 +29,33 @@ class Application(Frame):
 
     def inputDoneCallback(self, event):
         isRight = self.contents.get() == self.__curWord
-        self.__restorer.review("opposite",isRight)
+        self.__restorer.review(self.__curWord,isRight)
         if(isRight):
             self.__resultVar.set("Right!")
         else:
             self.__resultVar.set("Wrong!")
+            self.__addContent.insert(END, self.__curWord + linesep)
+
         if(len(self.__wordsList)!=0):
             self.__curWord = self.__wordsList.pop()
             self.__dic.pronounce(self.__curWord)
-
+        self.contents.set("")
+        self.__wordsInput.focus_set()
     def createWidgets(self):
-
+        labelInputNewWords = Label(text="Input words:")
+        labelInputNewWords.pack()
         # check if input word is correct
         self.__wordsInput = Entry()
         self.__wordsInput.pack()
         # here is the application variable
         self.contents = StringVar()
-        self.contents.set("input the word")
+        #self.contents.set("input the word")
         self.__wordsInput["textvariable"] = self.contents
         # and here we get a callback when the user hits return.
         # we will have the program print out the value of the
         # application variable when the user hits return
         self.__wordsInput.bind('<Key-Return>',
                                self.inputDoneCallback)
-
-        self.__correctTime = Entry()
-        self.__correctTime.pack()
-        # here is the application variable
-        self.__correctTimeVar = StringVar()
-        self.__correctTimeVar.set("input right time")
-        self.__correctTime["textvariable"] = self.__correctTimeVar
-
-        self.__reviewTime = Entry()
-        self.__reviewTime.pack()
-        # here is the application variable
-        self.__reviewTimeVar = StringVar()
-        self.__reviewTimeVar.set("input review time")
-        self.__reviewTime["textvariable"] = self.__reviewTimeVar
 
 
         # here is the application variable
@@ -67,10 +64,40 @@ class Application(Frame):
         self.__result = Label(textvariable=self.__resultVar)
         self.__result.pack()
 
-        self.__pronounce = Button(self)
-        self.__pronounce["text"] = "Pronounce",
-        self.__pronounce["command"] = self.say_hi
 
+        labelCorrectTime = Label(text="Correct Time:")
+        labelCorrectTime.pack()
+        self.__correctTime = Entry()
+        self.__correctTime.pack()
+
+
+
+        # here is the application variable
+        self.__correctTimeVar = StringVar()
+        self.__correctTimeVar.set("0")
+        self.__correctTime["textvariable"] = self.__correctTimeVar
+
+
+        labelReviewTime = Label(text="Review Time:")
+        labelReviewTime.pack()
+        self.__reviewTime = Entry()
+        self.__reviewTime.pack()
+        # here is the application variable
+        self.__reviewTimeVar = StringVar()
+        self.__reviewTimeVar.set("0")
+        self.__reviewTime["textvariable"] = self.__reviewTimeVar
+
+
+        labelNewWords = Label(text="Override/add words:")
+        labelNewWords.pack()
+        self.__addContent = Text()
+        self.__addContent.pack(expand=YES, fill=BOTH)
+
+
+
+        self.__pronounce = Button(self)
+        self.__pronounce["text"] = "Override Words"
+        self.__pronounce["command"] = self.overrideWordList
         self.__pronounce.pack({"side": "left"})
 
         self.__updateWords = Button(self)
@@ -108,7 +135,10 @@ class Application(Frame):
         self.__wordsList = []
         self.__curWord = ""
 
-    def __del__(self):
+    def destroy(self):
+        self.clearUp()
+
+    def clearUp(self):
         self.__restorer.update()
         self.__restorer.recordResult()
         with open(self.__configFile,'w') as fileObj:
